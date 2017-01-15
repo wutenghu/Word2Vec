9a36e003354bd8e2c532eb436a036035025d8b3f
Tenghu Wu, PhD <wutenghu.ubc@gmail.com>
2017-01-15 23:35:15 +0800

    english version word2vec

---
+++ english/eval_word2vec.py
@@ -0,0 +1,36 @@
+# Author: Tenghu Wu
+# Date: 2017-Jan-15
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from gensim.models import word2vec
+from wikipedia import search, page
+
+import logging
+import numpy as np
+import os
+import tensorflow as tf
+import time
+
+# set the global parameters here:
+timestamp = str(int(time.time()))
+flags = tf.app.flags
+flags.DEFINE_string('data_path', './data/ciities_corpus', 'path for training data')
+flags.DEFINE_string('save_path', 'runs/1484490925/checkpoints', 'path for saving data')
+flags.DEFINE_integer('min_count', 2, 'term occurs less than this is ignored')
+flags.DEFINE_integer('size', 50, 'embedding dimensions')
+flags.DEFINE_integer('window', 4, 'terms occur within a window-neighborhood of a term')
+flags.DEFINE_integer('sg', 1, 'sg=1:skip-gram model; sg=other:CBoW model')
+# flags.DEFINE_float()
+# flags.DEFINE_boolean()
+FLAGS = flags.FLAGS
+
+# the major part
+if __name__ == '__main__':
+    # load-in trained model
+    file_path = os.path.join(FLAGS.save_path,'UScities.model.bin')
+    model = word2vec.Word2Vec.load_word2vec_format(file_path, binary=True)
+
+    # test example 1:
+    print(model.most_similar('mountain'))
